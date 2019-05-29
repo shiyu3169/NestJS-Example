1742590b908f7c3c40c005bb46ba117e9a8f6ca4
Shiyu Wang <shiyu3169@gmail.com>
2019-05-29 14:58:51 -0400

    items

---
+++ src/items/items.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
+import {CreateItemDto} from "./dto/create-item.dto"
+
+@Controller('items')
+export class ItemsController {
+  @Get()
+  findAll(): string {
+    return 'Get all items';
+  }
+
+  @Post()
+  create(@Body() createItemDto: CreateItemDto): string {
+    return `Name: ${createItemDto.name} Description: ${createItemDto.description}`
+  }
+}
